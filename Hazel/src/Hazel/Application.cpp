449bcbbe8af952e6e2c27d3e5ce01579f4475303
Sterling Salvaterra <svsalvaterra@gmail.com>
2021-04-19 14:31:28 -0700

    Set up glfwWindow Events

--- Hazel/src/Hazel/Application.cpp
+++ Hazel/src/Hazel/Application.cpp
@@ -1,13 +1,15 @@
 #include "hzpch.h"
 #include "Application.h"
 
-#include "Hazel/Events/ApplicationEvent.h"
 #include "Hazel/Log.h"
 
 #include <GLFW/glfw3.h>
 
 
 namespace Hazel {
+
+#define BIND_EVENT_FN(x) std::bind(&Application::x, this, std::placeholders::_1)
+
 	Application::Application()
 	{
 		//Application is constructed
@@ -15,10 +17,21 @@ namespace Hazel {
 		//		which calls the WindowsWindow constructor, 
 		//			which calls WindowsWindo::Init()
 		m_Window = std::unique_ptr<Window>(Window::Create());
+		
+		//Bind which function is called OnEventCallback
+		m_Window->SetEventCallback(BIND_EVENT_FN(OnEvent));
 	}
 
 	Application::~Application()
 	{
+	}
+	
+	void Application::OnEvent(Event& e)
+	{
+		EventDispatcher dispatcher(e);
+		dispatcher.Dispatch<WindowCloseEvent>(BIND_EVENT_FN(OnWindowClose));
+
+		HZ_CORE_TRACE("{0}", e);
 	}
 
 	void Application::Run()
@@ -31,4 +44,9 @@ namespace Hazel {
 		}
 	}
 
+	bool Application::OnWindowClose(WindowCloseEvent& e)
+	{
+		m_Running = false;
+		return true;
+	}
 }//End namespace Hazel 
