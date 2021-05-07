95f631e0d22e6358ff7a1114daafdf95b0739f6a
Sterling Salvaterra <svsalvaterra@gmail.com>
2021-05-07 15:13:50 -0700

    Create ortho camera

--- Hazel/src/Hazel/Application.cpp
+++ Hazel/src/Hazel/Application.cpp
@@ -7,7 +7,6 @@
 
 #include "Input.h"
 
-
 namespace Hazel {
 
 #define BIND_EVENT_FN(x) std::bind(&Application::x, this, std::placeholders::_1)
@@ -15,6 +14,7 @@ namespace Hazel {
 	Application* Application::s_Instance = nullptr;
 
 	Application::Application()
+		:m_Camera(-1.6f, 1.6f, -0.9f, 0.9f)
 	{	
 		HZ_CORE_ASSERT(!s_Instance, "Application already exists!");
 		s_Instance = this;
@@ -87,6 +87,8 @@ namespace Hazel {
 			
 			layout(location = 0) in vec3 a_Position;
 			layout(location = 1) in vec4 a_Color;
+			
+			uniform mat4 u_ViewProjection;
 
 			out vec3 v_Position;
 			out vec4 v_Color;
@@ -95,7 +97,7 @@ namespace Hazel {
 			{
 				v_Position = a_Position;
 				v_Color = a_Color;
-				gl_Position = vec4(a_Position,1.0);
+				gl_Position = u_ViewProjection * vec4(a_Position,1.0);
 			}
 		)";
 
@@ -120,13 +122,15 @@ namespace Hazel {
 			#version 330 core
 			
 			layout(location = 0) in vec3 a_Position;
+			
+			uniform mat4 u_ViewProjection;
 
 			out vec3 v_Position;
 		
 			void main()
 			{
 				v_Position = a_Position;
-				gl_Position = vec4(a_Position,1.0);
+				gl_Position =  u_ViewProjection * vec4(a_Position,1.0);
 			}
 		)";
 
@@ -183,13 +187,13 @@ namespace Hazel {
 			RenderCommand::SetClearColor({ 0.1f, 0.1f, 0.1f, 1 });
 			RenderCommand::Clear();
 
-			Renderer::BeginScene();
+			m_Camera.SetPosition({ 0.5f,0.5f,0.5f });
+			m_Camera.SetRotation(45.0f);
+
+			Renderer::BeginScene(m_Camera);
 			
-			m_BlueShader->Bind();
-			Renderer::Submit(m_SquareVA);
-			
-			m_Shader->Bind();
-			Renderer::Submit(m_VertexArray);
+			Renderer::Submit( m_BlueShader,m_SquareVA);
+			Renderer::Submit(m_Shader, m_VertexArray);
 			
 			Renderer::EndScene();
 
--- Hazel/src/Hazel/Application.h
+++ Hazel/src/Hazel/Application.h
@@ -12,6 +12,7 @@
 #include "Hazel/Renderer/Shader.h"
 #include "Hazel/Renderer/Buffer.h"
 #include "Hazel/Renderer/VertexArray.h"
+#include "Hazel/Renderer/OrthographicCamera.h"
 
 namespace Hazel {
 	
@@ -43,6 +44,8 @@ namespace Hazel {
 	
 		std::shared_ptr<Shader> m_BlueShader;
 		std::shared_ptr<VertexArray> m_SquareVA;
+
+		OrthographicCamera m_Camera;
 	private:
 		static Application* s_Instance;
 	};
