2ce434f6e26d8cc9ea37ece4663dfe6eada8af38
Sterling Salvaterra <svsalvaterra@gmail.com>
2021-05-11 13:27:08 -0700

    Move code from app.cpp to sandbox.cpp

--- Hazel/src/Hazel.h
+++ Hazel/src/Hazel.h
@@ -12,5 +12,15 @@
 
 #include "Hazel/ImGui/ImGuiLayer.h"
 
-//----------Entry Point----------
+//---------- Renderer -------------
+#include "Hazel/Renderer/Renderer.h"
+#include "Hazel/Renderer/RenderCommand.h"
+
+#include "Hazel/Renderer/Buffer.h"
+#include "Hazel/Renderer/Shader.h"
+#include "Hazel/Renderer/VertexArray.h"
+
+#include "Hazel/Renderer/OrthographicCamera.h"
+
+//---------- Entry Point ----------
 #include "Hazel/EntryPoint.h"
